9a037718605f38007cbcd121f0041ac4cff21862
Kevin5771 <kevin577gutierrez@gmail.com>
2024-05-29 20:31:39 -0600

    Creación Conexión SQL

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=Dell_G15\\MSSQLSERVER01;Initial Catalog=BDCOLEGIO;Integrated Security=True";
+    }
+}
