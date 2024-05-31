4c622cc6e18e307b388e812da59d1798b4470b88
Kevin5771 <kevin577gutierrez@gmail.com>
2024-05-31 16:37:47 -0600

    Implementación de Interfaz de Usuario

--- Sistema/Login.Designer.cs
+++ Sistema/Login.Designer.cs
@@ -35,12 +35,12 @@
             // 
             this.labelTitle.AutoSize = true;
             this.labelTitle.BackColor = System.Drawing.Color.Transparent;
-            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTitle.ForeColor = System.Drawing.Color.White;
-            this.labelTitle.Location = new System.Drawing.Point(89, 25);
+            this.labelTitle.Location = new System.Drawing.Point(90, 20);
             this.labelTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(200, 46);
+            this.labelTitle.Size = new System.Drawing.Size(234, 54);
             this.labelTitle.TabIndex = 911;
             this.labelTitle.Text = "Bienvenido";
             // 
@@ -48,13 +48,14 @@
             // 
             this.btnIngresar.BackColor = System.Drawing.Color.CornflowerBlue;
             this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIngresar.FlatAppearance.BorderSize = 0;
             this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresar.ForeColor = System.Drawing.Color.White;
             this.btnIngresar.Location = new System.Drawing.Point(25, 285);
             this.btnIngresar.Margin = new System.Windows.Forms.Padding(4);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(331, 39);
+            this.btnIngresar.Size = new System.Drawing.Size(365, 45);
             this.btnIngresar.TabIndex = 908;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = false;
@@ -64,13 +65,14 @@
             // 
             this.btnSalir.BackColor = System.Drawing.Color.IndianRed;
             this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
             this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalir.ForeColor = System.Drawing.Color.White;
-            this.btnSalir.Location = new System.Drawing.Point(27, 332);
+            this.btnSalir.Location = new System.Drawing.Point(25, 345);
             this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(329, 39);
+            this.btnSalir.Size = new System.Drawing.Size(365, 45);
             this.btnSalir.TabIndex = 909;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = false;
@@ -78,45 +80,48 @@
             // 
             // txtClave
             // 
-            this.txtClave.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.txtClave.Location = new System.Drawing.Point(27, 223);
+            this.txtClave.BackColor = System.Drawing.Color.White;
+            this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtClave.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtClave.Location = new System.Drawing.Point(25, 232);
             this.txtClave.Margin = new System.Windows.Forms.Padding(4);
             this.txtClave.Name = "txtClave";
             this.txtClave.PasswordChar = '*';
-            this.txtClave.Size = new System.Drawing.Size(329, 34);
+            this.txtClave.Size = new System.Drawing.Size(365, 39);
             this.txtClave.TabIndex = 907;
             this.txtClave.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.txtUsuario.Location = new System.Drawing.Point(27, 153);
+            this.txtUsuario.BackColor = System.Drawing.Color.White;
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtUsuario.Location = new System.Drawing.Point(31, 157);
             this.txtUsuario.Margin = new System.Windows.Forms.Padding(4);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(329, 34);
+            this.txtUsuario.Size = new System.Drawing.Size(359, 39);
             this.txtUsuario.TabIndex = 904;
             this.txtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // labelClave
             // 
             this.labelClave.AutoSize = true;
-            this.labelClave.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.labelClave.Location = new System.Drawing.Point(24, 191);
+            this.labelClave.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.labelClave.Location = new System.Drawing.Point(25, 196);
             this.labelClave.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelClave.Name = "labelClave";
-            this.labelClave.Size = new System.Drawing.Size(110, 28);
+            this.labelClave.Size = new System.Drawing.Size(134, 32);
             this.labelClave.TabIndex = 905;
             this.labelClave.Text = "Contraseña";
-            this.labelClave.Click += new System.EventHandler(this.labelClave_Click);
             // 
             // labelUsuario
             // 
             this.labelUsuario.AutoSize = true;
-            this.labelUsuario.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.labelUsuario.Location = new System.Drawing.Point(24, 121);
+            this.labelUsuario.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.labelUsuario.Location = new System.Drawing.Point(25, 121);
             this.labelUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelUsuario.Name = "labelUsuario";
-            this.labelUsuario.Size = new System.Drawing.Size(79, 28);
+            this.labelUsuario.Size = new System.Drawing.Size(94, 32);
             this.labelUsuario.TabIndex = 906;
             this.labelUsuario.Text = "Usuario";
             // 
@@ -128,7 +133,7 @@
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Margin = new System.Windows.Forms.Padding(4);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(381, 98);
+            this.panelHeader.Size = new System.Drawing.Size(417, 90);
             this.panelHeader.TabIndex = 912;
             // 
             // Login
@@ -136,7 +141,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(381, 402);
+            this.ClientSize = new System.Drawing.Size(417, 471);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.txtClave);
@@ -149,7 +154,7 @@
             this.MaximizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Login";
+            this.Text = "Inicio de Sesión";
             this.Load += new System.EventHandler(this.Login_Load);
             this.panelHeader.ResumeLayout(false);
             this.panelHeader.PerformLayout();
